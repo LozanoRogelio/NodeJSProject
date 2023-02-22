d3a1dc31b67cf8437e9025ca2ccdea913c42545c
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-02-22 13:50:44 -0600

    Routing, redirects, and HTTP requests through Express

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-   res.send('<p>Home Page</p>');
+   // res.send('<p>Home Page</p>');
+   res.sendFile('./views/index.html', { root: __dirname})
+});
+app.get('/about', (req, res) => {
+   // res.send('<p>About Page</p>');
+   res.sendFile('./views/about.html', { root: __dirname})
+});
 
+// redirects
+app.get('/about-us', (req, res) => {
+   res.redirect('/about')
+})
+
+// 404 Page
+app.use((req, res) => {
+   res.status(404).sendFile('./views/404.html', { root: __dirname})
 })

4e00918cc971dc82cac36b39fc91ce40bdd28381
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-02-27 16:23:34 -0600

    Pass data from handler to view

--- app.js
+++ app.js
@@ -13,19 +13,20 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
    // res.send('<p>Home Page</p>');
-   res.sendFile('./views/index.html', { root: __dirname})
+   res.render('index');
 });
 app.get('/about', (req, res) => {
    // res.send('<p>About Page</p>');
-   res.sendFile('./views/about.html', { root: __dirname})
+   // res.sendFile('./views/about.html', { root: __dirname})
+   res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-   res.redirect('/about')
-});
+// new blog
+app.get('/blogs/create', (req, res) => {
+   res.render('create');
+})
 
 // 404 Page
 app.use((req, res) => {
-   res.status(404).sendFile('./views/404.html', { root: __dirname })
+   res.status(404).render('404');
 });

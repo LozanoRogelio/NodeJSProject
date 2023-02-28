1c3c5c8f499e746007195ac430cf26c6e730828d
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-02-28 14:01:35 -0600

    Created partials and used ejs fundamentals

--- app.js
+++ app.js
@@ -13,20 +13,25 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
    // res.send('<p>Home Page</p>');
-   res.render('index');
+   const blogs = [
+      {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+   res.render('index', { title: 'Home', blogs});
 });
 app.get('/about', (req, res) => {
    // res.send('<p>About Page</p>');
    // res.sendFile('./views/about.html', { root: __dirname})
-   res.render('about');
+   res.render('about', { title: 'About'});
 });
 
 // new blog
 app.get('/blogs/create', (req, res) => {
-   res.render('create');
+   res.render('create', { title: 'Create a new blog'});
 })
 
 // 404 Page
 app.use((req, res) => {
-   res.status(404).render('404');
+   res.status(404).render('404', { title: '404'});
 });

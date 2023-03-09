951d6eae4a2b221e4cb1bdfd46a08170c71c6216
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-03-09 06:34:56 -0600

    Using mongoose and creating schemas

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+
 // express app
 const app = express();
+// connect to mongodb
+const dbURI = 'mongodb+srv://roger:test1234@nodetuts.ecrbltj.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }).then((result) => app.listen(3000)).catch((err) => console.log(err))
 
 // register view engine
 
@@ -9,7 +15,7 @@ app.set('view engine', 'ejs');
 
 
 // listen for requests
-app.listen(3000);
+
 
 // middleware & static file
 app.use(express.static('public'));
